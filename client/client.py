7c4c59374e2fe0465bd717712f904d8982298778
mzd245 <seyedimanseyedarab2003@gmail.com>
2020-03-24 05:59:22 +0430

    Client: 'Spam' and 'Lemme google it for you' features added to client.

--- client/client.py
+++ client/client.py
@@ -1,5 +1,8 @@
 from pyrogram import Client, Filters
+from pyrogram.errors import FloodWait
 import json
+import time 
+
 
 # User Authorization
 app = Client("my_account")
@@ -21,6 +24,35 @@ def exec_admin_command(command):
             data[command]
         )
 
+# Spam
+@app.on_message(
+    Filters.command("spam", "!") &
+    Filters.me)
+def spam(client, message):
+    try:
+        for _ in range(int(message.command[1])):
+            app.send_message(
+                message.chat.id,
+                ' '.join(message.command[2:])
+            )
+    except FloodWait as e:
+        time.sleep(e.x)
+
+# Let me google it for you!
+@app.on_message(
+    Filters.command("g", "!") &
+    Filters.me)
+def google(client, message):
+    text = message.command[1:]
+    google_it = "http://google.com/search?q=" + '+'.join(text)
+    edited_message = """Let me 🔎 Google that for you:
+🔎[{}]({})""".format(' '.join(text), google_it) 
+    app.edit_message_text(
+        message.chat.id,
+        message.message_id,
+        edited_message,
+        disable_web_page_preview=True
+    )
 
 commands =  data.keys()
 for command in commands:
